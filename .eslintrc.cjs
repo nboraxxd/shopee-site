7d8b16bbd75a94cf9442a9c48bb71e2e764b54d8
bora <nhatbaoxxd@gmail.com>
2023-08-01 01:35:36 +0700

    Setup project
    - Eslint
    - Prettier
    - Tailwind
    - some config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,28 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'eslint-config-prettier',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier'],
   rules: {
-    'react-refresh/only-export-components': [
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'prettier/prettier': [
       'warn',
-      { allowConstantExport: true },
+      {
+        arrowParens: 'always',
+        semi: false,
+        trailingComma: 'es5',
+        tabWidth: 2,
+        endOfLine: 'auto',
+        useTabs: false,
+        singleQuote: true,
+        printWidth: 120,
+        jsxSingleQuote: false,
+      },
     ],
   },
-}
+  ignorePatterns: ['.eslintrc.cjs', 'vite.config.ts'],
+};
