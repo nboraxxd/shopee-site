884c419ba5fc8a400a49e22a7777b2a1ccb14b28
bora <nhatbaoxxd@gmail.com>
2023-08-03 05:51:15 +0700

    Handle message errors with axios interceptor

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useRoutes } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { routers } from '@/routers'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   const queryClient = new QueryClient({
@@ -16,6 +18,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       {element}
+      <ToastContainer />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   )
--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,7 +1,23 @@
-import axios, { type AxiosInstance } from 'axios'
+import HttpStatusCode from '@/constants/httpStatusCode.enum'
+import axios, { AxiosError, type AxiosInstance } from 'axios'
+import { toast } from 'react-toastify'
 
 export const http: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_SERVER_URL,
   timeout: 10000,
   headers: { 'Content-Type': 'application/json' },
 })
+
+http.interceptors.response.use(
+  function (response) {
+    return response
+  },
+  function (error: AxiosError) {
+    console.log(error)
+    if (error.response?.status !== HttpStatusCode.UnprocessableEntity) {
+      const data: any | undefined = error.response?.data
+      const message = data.message || error.message
+      toast.error(message)
+    }
+  }
+)
