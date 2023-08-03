407f549cd4db7e6d45c58e077e557cadb4074e69
bora <nhatbaoxxd@gmail.com>
2023-08-03 13:40:22 +0700

    Code popover using Framer Motion and Floating UI

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,31 @@
+import { useRef, useState } from 'react'
+import { FloatingArrow, FloatingPortal, arrow, offset, shift, useFloating } from '@floating-ui/react'
 import { Link } from 'react-router-dom'
+import { motion, AnimatePresence } from 'framer-motion'
 import { PATH } from '@/config/path'
 
 export default function Header() {
+  const TOOLTIP_GAP = 7
+  const [isOpen, setIsOpen] = useState(false)
+  const arrowRef = useRef(null)
+  const { refs, strategy, x, y, context, middlewareData } = useFloating({
+    middleware: [
+      shift(),
+      arrow({
+        element: arrowRef,
+      }),
+      offset(TOOLTIP_GAP),
+    ],
+  })
+
+  const showPopover = () => {
+    setIsOpen(true)
+  }
+
+  const hidePopover = () => {
+    setIsOpen(false)
+  }
+
   return (
     <header className="bg-[linear-gradient(-180deg,#f53d2d,#F63)] pb-5 pt-2">
       {/* Container */}
@@ -9,32 +33,73 @@ export default function Header() {
         {/* Top Bar Menu */}
         <ul className="-mx-3 flex justify-end">
           {/* Language */}
-          <li className="flex cursor-pointer items-center px-3 py-1 text-gray-50 transition-all hover:text-gray-200">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="h-5 w-5"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418"
-              />
-            </svg>
-            <span className="mx-1 text-sm">Tiếng Việt</span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={2}
-              stroke="currentColor"
-              className="h-4 w-4"
-            >
-              <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-            </svg>
+          <li className="relative" ref={refs.setReference} onMouseEnter={showPopover} onMouseLeave={hidePopover}>
+            <div className="flex cursor-pointer items-center px-3 py-1 text-gray-50 transition-all hover:text-gray-200">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="h-5 w-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418"
+                />
+              </svg>
+              <span className="mx-1 text-sm">Tiếng Việt</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={2}
+                stroke="currentColor"
+                className="h-4 w-4"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+              </svg>
+            </div>
+            <div className=" absolute left-1/2 h-3 w-[114px] -translate-x-1/2"></div>
+            {/* Tooltip */}
+            <AnimatePresence>
+              {isOpen && (
+                <FloatingPortal>
+                  <motion.div
+                    ref={refs.setFloating}
+                    style={{
+                      position: strategy,
+                      top: y ?? 0,
+                      left: x ?? 0,
+                      width: 'max-content',
+                      transformOrigin: `${middlewareData.arrow?.x}px top`,
+                    }}
+                    initial={{ opacity: 0, scale: 0.5 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    exit={{ opacity: 0, scale: 0 }}
+                    transition={{
+                      duration: 0.2,
+                    }}
+                  >
+                    <FloatingArrow
+                      ref={arrowRef}
+                      context={context}
+                      fill="white"
+                      height={10}
+                      width={20}
+                      staticOffset={30}
+                      style={{ zIndex: 1, bottom: '99%' }}
+                    />
+                    <div className="flex flex-col rounded-sm border border-gray-200 bg-white shadow-md">
+                      <button className="px-6 py-3 hover:text-primary">Tiếng Việt</button>
+                      <button className="border-t px-6 py-3 hover:text-primary">English</button>
+                    </div>
+                  </motion.div>
+                </FloatingPortal>
+              )}
+            </AnimatePresence>
+            {/* End Tooltip */}
           </li>
           {/* End Language */}
           {/* Account */}
@@ -105,7 +170,7 @@ export default function Header() {
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              strokeWidth={2}
+              strokeWidth={1.8}
               stroke="currentColor"
               className="h-8 w-8 text-white"
             >
