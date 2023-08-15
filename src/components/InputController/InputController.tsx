a0388fae9c6d61b162dba8bef93a5225144a6747
bora <nhatbaoxxd@gmail.com>
2023-08-15 16:14:10 +0700

    Use useController to generate InputController

---
+++ src/components/InputController/InputController.tsx
@@ -0,0 +1,56 @@
+import { ChangeEvent, InputHTMLAttributes, useState } from 'react'
+import { FieldPath, FieldValues, UseControllerProps, useController } from 'react-hook-form'
+
+export type InputControllerProps<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+> = {
+  classNameWrapper?: string
+  classNameInput?: string
+  classNameError?: string
+} & InputHTMLAttributes<HTMLInputElement> &
+  UseControllerProps<TFieldValues, TName>
+
+export default function InputController<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+>(props: InputControllerProps<TFieldValues, TName>) {
+  const {
+    type,
+    onChange,
+    classNameWrapper = 'mt-2',
+    classNameInput = 'w-full p-3 focus:border-gray-400',
+    classNameError = 'min-h-[1rem]',
+    value = '',
+    ...rest
+  } = props
+
+  const { field, fieldState } = useController(props)
+  const [localValue, setLocalValue] = useState<string>(field.value)
+
+  function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+    const valueFromInput = event.target.value
+    const numberCondition = type === 'number' && (/^\d+$/.test(valueFromInput) || valueFromInput === '')
+    if (numberCondition || type !== 'number') {
+      // Cập nhật localValue state
+      setLocalValue(valueFromInput)
+      // Gọi field.onChange để cập nhật vào state React Hook Form
+      field.onChange(event)
+      // Thực thi onChange callback từ bên ngoài truyền vào props
+      onChange && onChange(event)
+    }
+  }
+
+  return (
+    <div className={classNameWrapper}>
+      <input
+        className={`rounded-sm border border-gray-300 outline-none focus:shadow-sm ${classNameInput}`}
+        {...rest}
+        {...field}
+        onChange={handleInputChange}
+        value={localValue || value}
+      />
+      <p className={`mt-1 text-xs text-red-600 ${classNameError}`}>{fieldState.error?.message}</p>
+    </div>
+  )
+}
--- src/pages/ProductList/components/AsideFilter/AsideFilter.tsx
+++ src/pages/ProductList/components/AsideFilter/AsideFilter.tsx
@@ -15,6 +15,7 @@ import { trimLeadingZeros } from '@/utils/utils'
 import { RatingStars } from '@/pages/ProductList'
 import { Button } from '@/components/Button'
 import { InputNumber } from '@/components/InputNumber'
+import { InputController } from '@/components/InputController'
 
 interface Props {
   isShowAside: boolean
@@ -250,27 +251,18 @@ export default function AsideFilter(props: Props) {
             <p className="text-sm">Khoảng giá</p>
             <form className="mt-2" onSubmit={onSubmit}>
               <div className="flex items-center">
-                <Controller
+                <InputController
                   control={control}
                   name="price_min"
-                  render={({ field }) => {
-                    return (
-                      <InputNumber
-                        classNameWrapper="grow"
-                        classNameInput="w-full p-[4.5px_2px_4.5px_5px] placeholder:text-xs"
-                        classNameError="min-h-0"
-                        placeholder="₫ TỪ"
-                        onChange={(event) => {
-                          field.onChange(event)
-                          trigger('price_max')
-                        }}
-                        value={field.value}
-                        ref={field.ref}
-                      />
-                    )
+                  type="number"
+                  classNameWrapper="grow"
+                  classNameInput="w-full p-[4.5px_2px_4.5px_5px] placeholder:text-xs"
+                  classNameError="min-h-0"
+                  placeholder="₫ TỪ"
+                  onChange={() => {
+                    trigger('price_max')
                   }}
                 />
-
                 <div className="mx-3 h-[1px] w-3 shrink-0 bg-[#bdbdbd]" />
                 <Controller
                   control={control}
@@ -282,6 +274,7 @@ export default function AsideFilter(props: Props) {
                         classNameInput="w-full p-[4.5px_2px_4.5px_5px] placeholder:text-xs"
                         classNameError="min-h-0"
                         placeholder="₫ ĐẾN"
+                        {...field}
                         onChange={(ev) => {
                           field.onChange(ev)
                           trigger('price_min')
