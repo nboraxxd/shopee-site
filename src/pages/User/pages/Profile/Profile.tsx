28cb8922b2b8ab7cb59aa5b8da7b0fe4981166d8
bora <nhatbaoxxd@gmail.com>
2023-08-21 19:36:20 +0700

    Usage useFormContext in React Hook Form

--- src/pages/User/pages/Profile/Profile.tsx
+++ src/pages/User/pages/Profile/Profile.tsx
@@ -1,7 +1,7 @@
 import { yupResolver } from '@hookform/resolvers/yup'
 import { useMutation, useQuery } from '@tanstack/react-query'
-import { useContext, useEffect, useState } from 'react'
-import { Controller, useForm } from 'react-hook-form'
+import { Fragment, useContext, useEffect, useState } from 'react'
+import { Controller, FormProvider, useForm, useFormContext } from 'react-hook-form'
 import { toast } from 'react-toastify'
 
 import userApi, { type BodyUpdateProfile } from '@/apis/user.api'
@@ -17,7 +17,39 @@ import { Input } from '@/components/Input'
 import { InputFile } from '@/components/InputFile'
 import { DateSelect } from '@/pages/User/components/DateSelect'
 
-// type ProfileSchema = Pick<UserSchema, 'name' | 'phone' | 'address' | 'date_of_birth' | 'avatar'>
+function Info() {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext<ProfileSchema>()
+
+  return (
+    <Fragment>
+      <div className="mt-2 flex flex-col md:flex-row md:items-center md:gap-2 lg:gap-5">
+        <div className="mb-[0.125rem] text-gray-500 md:mb-5 md:w-1/4 md:text-right lg:w-1/5">Tên</div>
+        <Input
+          classNameWrapper="md:w-3/4 lg:w-4/5 w-full"
+          classNameInput="px-3 py-2"
+          placeholder="Bruce Wayne"
+          name="name"
+          register={register}
+          errorMessage={errors.name?.message}
+        />
+      </div>
+      <div className="mt-2 flex flex-col md:flex-row md:items-center md:gap-2 lg:gap-5">
+        <div className="mb-[0.125rem] text-gray-500 md:mb-5 md:w-1/4 md:text-right lg:w-1/5">Số điện thoại</div>
+        <Input
+          classNameWrapper="md:w-3/4 lg:w-4/5 w-full"
+          classNameInput="px-3 py-2"
+          placeholder="0987***321"
+          name="phone"
+          register={register}
+          errorMessage={errors.phone?.message}
+        />
+      </div>
+    </Fragment>
+  )
+}
 
 type ProfileSchema = {
   name: string | undefined
@@ -44,15 +76,7 @@ export default function Profile() {
 
   const uploadAvatarMutation = useMutation({ mutationFn: (body: FormData) => userApi.updateAvatar(body) })
 
-  const {
-    register,
-    setValue,
-    control,
-    handleSubmit,
-    formState: { errors },
-    setError,
-    watch,
-  } = useForm<ProfileSchema>({
+  const methods = useForm<ProfileSchema>({
     defaultValues: {
       name: '',
       phone: '',
@@ -62,6 +86,16 @@ export default function Profile() {
     },
     resolver: yupResolver(profileSchema),
   })
+
+  const {
+    register,
+    setValue,
+    control,
+    handleSubmit,
+    formState: { errors },
+    setError,
+    watch,
+  } = methods
 
   const avatar = watch('avatar')
 
@@ -122,79 +156,64 @@ export default function Profile() {
         <h1 className="text-lg font-medium text-gray-700">Hồ Sơ Của Tôi</h1>
         <p className="mt-1 hidden text-sm text-gray-500 md:block">Quản lý thông tin hồ sơ để bảo mật tài khoản</p>
       </div>
-      <form className="mt-2.5 flex flex-col-reverse md:mt-5 md:flex-row md:items-start" onSubmit={onSubmit}>
-        <div className="mt-6 grow md:mt-0 md:pr-3 lg:pr-12">
-          <div className="flex flex-col md:flex-row md:items-center md:gap-2 lg:gap-5">
-            <div className="mb-[0.125rem] text-gray-500 md:mb-5 md:w-1/4 md:text-right lg:w-1/5">Email</div>
-            <Input
-              classNameWrapper="md:w-3/4 lg:w-4/5 w-full"
-              classNameInput="px-3 py-2"
-              disabled
-              defaultValue={profileData?.email}
+      <FormProvider {...methods}>
+        <form className="mt-2.5 flex flex-col-reverse md:mt-5 md:flex-row md:items-start" onSubmit={onSubmit}>
+          <div className="mt-6 grow md:mt-0 md:pr-3 lg:pr-12">
+            <div className="flex flex-col md:flex-row md:items-center md:gap-2 lg:gap-5">
+              <div className="mb-[0.125rem] text-gray-500 md:mb-5 md:w-1/4 md:text-right lg:w-1/5">Email</div>
+              <Input
+                classNameWrapper="md:w-3/4 lg:w-4/5 w-full"
+                classNameInput="px-3 py-2"
+                disabled
+                defaultValue={profileData?.email}
+              />
+            </div>
+            <Info />
+            <div className="mt-2 flex flex-col md:flex-row md:items-center md:gap-2 lg:gap-5">
+              <div className="mb-[0.125rem] text-gray-500 md:mb-5 md:w-1/4 md:text-right lg:w-1/5">Địa chỉ</div>
+              <Input
+                classNameWrapper="md:w-3/4 lg:w-4/5 w-full"
+                classNameInput="px-3 py-2"
+                placeholder="Gotham city"
+                name="address"
+                register={register}
+                errorMessage={errors.address?.message}
+              />
+            </div>
+            <Controller
+              control={control}
+              name="date_of_birth"
+              render={({ field }) => (
+                <DateSelect
+                  errorMessage={errors.date_of_birth?.message}
+                  onChange={field.onChange}
+                  value={field.value}
+                />
+              )}
             />
-          </div>
-          <div className="mt-2 flex flex-col md:flex-row md:items-center md:gap-2 lg:gap-5">
-            <div className="mb-[0.125rem] text-gray-500 md:mb-5 md:w-1/4 md:text-right lg:w-1/5">Tên</div>
-            <Input
-              classNameWrapper="md:w-3/4 lg:w-4/5 w-full"
-              classNameInput="px-3 py-2"
-              placeholder="Bruce Wayne"
-              name="name"
-              register={register}
-              errorMessage={errors.name?.message}
-            />
-          </div>
-          <div className="mt-2 flex flex-col md:flex-row md:items-center md:gap-2 lg:gap-5">
-            <div className="mb-[0.125rem] text-gray-500 md:mb-5 md:w-1/4 md:text-right lg:w-1/5">Số điện thoại</div>
-            <Input
-              classNameWrapper="md:w-3/4 lg:w-4/5 w-full"
-              classNameInput="px-3 py-2"
-              placeholder="0987***321"
-              name="phone"
-              register={register}
-              errorMessage={errors.phone?.message}
-            />
-          </div>
-          <div className="mt-2 flex flex-col md:flex-row md:items-center md:gap-2 lg:gap-5">
-            <div className="mb-[0.125rem] text-gray-500 md:mb-5 md:w-1/4 md:text-right lg:w-1/5">Địa chỉ</div>
-            <Input
-              classNameWrapper="md:w-3/4 lg:w-4/5 w-full"
-              classNameInput="px-3 py-2"
-              placeholder="Gotham city"
-              name="address"
-              register={register}
-              errorMessage={errors.address?.message}
-            />
-          </div>
-          <Controller
-            control={control}
-            name="date_of_birth"
-            render={({ field }) => (
-              <DateSelect errorMessage={errors.date_of_birth?.message} onChange={field.onChange} value={field.value} />
-            )}
-          />
-          <div className="mt-4 flex flex-col md:mt-2 md:flex-row md:items-center md:gap-2 lg:gap-5">
-            <div className="mb-[0.125rem] text-gray-500 md:mb-0 md:w-1/4 md:text-right lg:w-1/5"></div>
-            <div className="md:w-3/4 lg:w-4/5">
-              <Button className="rounded-sm px-5 py-2.5 text-base" type="submit" disabled={profileMutation.isLoading}>
-                Lưu
-              </Button>
+            <div className="mt-4 flex flex-col md:mt-2 md:flex-row md:items-center md:gap-2 lg:gap-5">
+              <div className="mb-[0.125rem] text-gray-500 md:mb-0 md:w-1/4 md:text-right lg:w-1/5"></div>
+              <div className="md:w-3/4 lg:w-4/5">
+                <Button className="rounded-sm px-5 py-2.5 text-base" type="submit" disabled={profileMutation.isLoading}>
+                  Lưu
+                </Button>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="md:border-l md:border-l-gray-200 md:pl-1 lg:px-5">
-          <div className="flex flex-col items-center">
-            <div className="my-4 h-24 w-24 overflow-hidden rounded-full">
-              <AvatarImage file={file} />
-            </div>
-            <InputFile onChange={onChangeFile} />
-            <div className="mt-3 md:text-[0.625rem] lg:text-sm">
-              <p>Dung lượng file tối đa 1MB</p>
-              <p>Định dạng: .JPEG, .PNG</p>
+          <div className="md:border-l md:border-l-gray-200 md:pl-1 lg:px-5">
+            <div className="flex flex-col items-center">
+              <div className="my-4 h-24 w-24 overflow-hidden rounded-full">
+                <AvatarImage file={file} />
+              </div>
+              <InputFile onChange={onChangeFile} />
+              <div className="mt-3 md:text-[0.625rem] lg:text-sm">
+                <p>Dung lượng file tối đa 1MB</p>
+                <p>Định dạng: .JPEG, .PNG</p>
+              </div>
             </div>
           </div>
-        </div>
-      </form>
+        </form>
+      </FormProvider>
     </div>
   )
 }
