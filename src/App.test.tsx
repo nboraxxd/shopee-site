2ba8aa1bef32fdbe2aaabd470f0eb4c51b41e873
bora <nhatbaoxxd@gmail.com>
2023-08-26 09:02:42 +0700

    Test not found page

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,9 @@
 import { describe, expect, it } from 'vitest'
 import { render, screen, waitFor } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import '@testing-library/jest-dom'
-import { BrowserRouter } from 'react-router-dom'
+// import userEvent from '@testing-library/user-event'
+// import '@testing-library/jest-dom'
+import { BrowserRouter, MemoryRouter } from 'react-router-dom'
+import { logScreen } from '@/utils/__test__/testUtils'
 import App from '@/App'
 
 describe('App', () => {
@@ -10,10 +11,10 @@ describe('App', () => {
     render(<App />, {
       wrapper: BrowserRouter,
     })
-    const user = userEvent.setup()
+    // const user = userEvent.setup()
     /**
      * waitFor sẽ run callback 1 vài lần
-     * cho đến khi hết timeout
+     * cho đến khi hết timeout hoặc expect pass
      * số lần run phụ thuộc vào timeout và interval
      * mặc định: timeout = 1000ms và interval = 50ms
      */
@@ -24,9 +25,20 @@ describe('App', () => {
     })
 
     // Verify chuyển sang trang login
-    await user.click(screen.getByText(/Đăng Nhập/i))
+    // await user.click(screen.getByText(/Đăng Nhập/i))
     // await waitFor(() => {})
 
+    screen.debug(document.body.parentElement as HTMLElement, 99999999)
+  })
+
+  it('Redirect sang trang not found', async () => {
+    const badRoute = '/some/bad/route'
+    render(
+      <MemoryRouter initialEntries={[badRoute]}>
+        <App />
+      </MemoryRouter>
+    )
+    await logScreen()
     screen.debug(document.body.parentElement as HTMLElement, 99999999)
   })
 })
